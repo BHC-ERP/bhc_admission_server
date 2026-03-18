e3599508bf74a507db6106ee0f4aadfe4e524d7e
balasundar-its-bhc <balasundar@bhc.edu.in>
2026-03-19 02:09:36 +0530

    UPDATED SMS

--- src/services/sms.service.ts
+++ src/services/sms.service.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { env } from "../config/env";
 
 export const sendSMSService = async (
     mobile: string,
     message: string
 ): Promise<void> => {
     try {
-        const url = `${process.env.SMS_BASE_URL}?username=${process.env.SMS_USERNAME}&apikey=${process.env.SMS_API_KEY}&senderid=${process.env.SMS_SENDER_ID}&mobile=${mobile}&message=${encodeURIComponent(
+        const url = `${env.SMS_BASE_URL}?username=${env.SMS_USERNAME}&apikey=${env.SMS_API_KEY}&senderid=${env.SMS_SENDER_ID}&mobile=${mobile}&message=${encodeURIComponent(
             message
         )}`;
 
